b2cd0efe61c4eb6d82ef8caa53d1a2c5bd6eb6ec
Jesse Luoto <jesse.luoto@gmail.com>
2016-10-06 18:42:50 +0300

    Added: easeljs

--- client/main.js
+++ client/main.js
@@ -4,9 +4,11 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Players } from '../imports/api/players'
 import { randomHex } from '../imports/helpers'
 import './main.html';
+require('createjs-easeljs');
 
 Template.body.onCreated(() => {
   setTimeout(() => {
+    const stage = new createjs.Stage('canvas');
     const canvas = document.getElementById('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -15,13 +17,7 @@ Template.body.onCreated(() => {
     const players = {};
 
     function render() {
-      ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
-      Object.keys(players).map((k) => players[k]).forEach((player) => {
-        ctx.beginPath();
-        ctx.fillStyle = player.color;
-        ctx.arc(player.x, player.y, 20, 0, 2*Math.PI);
-        ctx.fill();
-      });
+      stage.update();
     }
 
     const myID = localStorage.myID || (localStorage.myID = randomHex(50))
@@ -29,28 +25,35 @@ Template.body.onCreated(() => {
       Meteor.call('createPlayer', myID)
     }
 
-    function move(x, y) {
-      Meteor.call('move', x, y, myID);
+    function move(x, y, r) {
+      Meteor.call('move', x, y, r, myID);
     }
 
     setInterval(function updatePhysics() {
       var amountX = 0;
       var amountY = 0;
+      var amountRot = 0;
 
-      if (Input.getKeyDown(Input.keys.up)) {
+      if (Input.getKeyDown(Input.keys.w)) {
         amountY--;
-      } else if (Input.getKeyDown(Input.keys.down)) {
+      } else if (Input.getKeyDown(Input.keys.s)) {
         amountY++;
       }
 
-      if (Input.getKeyDown(Input.keys.left)) {
+      if (Input.getKeyDown(Input.keys.a)) {
         amountX--;
-      } else if (Input.getKeyDown(Input.keys.right)) {
+      } else if (Input.getKeyDown(Input.keys.d)) {
         amountX++;
       }
 
-      if (amountX || amountY) {
-        move(amountX, amountY);
+      if (Input.getKeyDown(Input.keys.left)) {
+        amountRot--;
+      } else if (Input.getKeyDown(Input.keys.right)) {
+        amountRot++;
+      }
+
+      if (amountX || amountY || amountRot) {
+        move(amountX, amountY, amountRot);
       }
     }, 1000 / 60);
 
@@ -68,7 +71,11 @@ Template.body.onCreated(() => {
         down: 40,
         left: 37,
         right: 39,
-        space: 32
+        space: 32,
+        a: 65,
+        s: 83,
+        d: 68,
+        w: 87
       },
       getKeyDown(key) {
         return Input.keysDown[key];
@@ -82,17 +89,30 @@ Template.body.onCreated(() => {
       Input.keysDown[e.which] = false;
     });
 
+    class Player {
+      constructor(data) {
+        this.sprite = new createjs.Shape();
+        this.sprite.graphics.beginFill(data.color).drawRect(-15, -15, 30, 30);
+        stage.addChild(this.sprite);
+
+        this.setData(data);
+      }
+
+      setData(data) {
+        this.data = data;
+        this.sprite.x = data.x;
+        this.sprite.y = data.y;
+        this.sprite.rotation = data.rotation;
+      }
+    }
+
     Players.find({}).observe({
-      addedAt: function(player, idx) {
-        players[idx] = player;
+      addedAt: function(data, idx) {
+        players[idx] = new Player(data);
         render();
       },
-      changedAt: function(player, _, idx) {
-        players[idx] = player;
-        render();
-      },
-      removedAt(_, idx) {
-        delete players[idx];
+      changedAt: function(data, _, idx) {
+        players[idx].setData(data);
         render();
       }
     });
@@ -104,17 +124,3 @@ Template.body.helpers({
     return Players.find({});
   },
 });
-/*
-Template.hello.helpers({
-counter() {
-return Template.instance().counter.get();
-},
-});
-
-Template.hello.events({
-'click button'(event, instance) {
-// increment the counter when button is clicked
-instance.counter.set(instance.counter.get() + 1);
-},
-});
-*/
--- server/main.js
+++ server/main.js
@@ -6,11 +6,24 @@ const speed = 2;
 
 Meteor.startup(() => {
   Meteor.methods({
-    move(x, y, idx) {
-      Players.update({_id: idx}, {$inc: {y: y * speed, x: x * speed}})
+    move(mx, my, mr, idx) {
+      const player = Players.find({_id: idx}).fetch()[0];
+      const {x, y, rotation} = player;
+      const r = rotation + mr*speed;
+      const [nx, ny] = rotate(x, y, x + (mx * speed), y + (my * speed), -r);
+      Players.update({_id: idx}, {$set: {y: ny, x: nx, rotation: r}})
     },
     createPlayer(idx) {
-      Players.insert({_id: idx, x:100, y: 100, color: '#' + randomHex(6)})
+      Players.insert({_id: idx, x:100, y: 100, rotation: 0, color: '#' + randomHex(6)})
     }
   });
 });
+
+function rotate(cx, cy, x, y, angle) {
+    var radians = (Math.PI / 180) * angle,
+        cos = Math.cos(radians),
+        sin = Math.sin(radians),
+        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+    return [nx, ny];
+}
